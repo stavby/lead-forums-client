45ec03ac8dbb9c89b00000df35450331106afb7b
snat23 <snat23@proton.me>
2023-02-15 22:54:15 +0200

    mvp of TopicCards

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,5 +1,48 @@
+import Box from '@mui/material/Box/Box';
 import React from 'react';
+import TopicCard from './TopicCard';
 
-const HomePage = () => <>Homepage</>;
+const HomePage = () => (
+  <>
+    <Box
+      margin={1}
+      padding={1}
+      display={'flex'}
+      flexDirection={'row-reverse'}
+      borderRadius={1}
+    >
+      <Box margin={2}>
+        <TopicCard
+          image={'/resources/img1.jpg'}
+          topicName={'מנהיגות'}
+          topicDescription={
+            'מהי מנהיגות? כיצד ניצן לממש אותה? פורום ההנהלה כאן בשבילכם'
+          }
+        />
+      </Box>
+      <Box margin={2}>
+        <TopicCard
+          image={'/resources/img2.jpg'}
+          topicName={'עבודה'}
+          topicDescription={'חיפוש עובדים ומשרות פנויות'}
+        />
+      </Box>{' '}
+      <Box margin={2}>
+        <TopicCard
+          image={'/resources/img3.jpg'}
+          topicName={'לימודים ומלגות'}
+          topicDescription={'כל מה שתצטרכו לדעת לגבי הלימודים שלכם'}
+        />
+      </Box>
+      <Box margin={2}>
+        <TopicCard
+          image={'/resources/img4.jpg'}
+          topicName={'פרויקטים'}
+          topicDescription={'פורום הפרויקטים הגדול. ניהול, הובלה, ייעוץ ועוד!'}
+        />
+      </Box>
+    </Box>
+  </>
+);
 
 export default HomePage;
