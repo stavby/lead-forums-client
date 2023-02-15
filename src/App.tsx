431174df64ae95285024f2cc96457438646f2874
lior <liorp10087@gmail.com>
2023-02-15 17:42:51 +0200

    init google login

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,20 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Bar from './components/Bar';
 import ProfilePicture from './components/ProfilePicture';
+import { CredentialResponse, GoogleLogin } from '@react-oauth/google';
 import './App.css';
 
 const App = () => (
+
   <BrowserRouter>
+    <div>
+      <h2>React Google Login</h2>
+      <br />
+      <br />
+      <GoogleLogin onSuccess={function (credentialResponse: CredentialResponse): void {
+          console.log(credentialResponse);
+      } } />
+    </div>
     <Routes>
       <Route path='/' element={<Bar />}>
         <Route index element={<div></div>} />
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId="41662238799-f8f0104b181hn9e5uec7vvtspefdoljt.apps.googleusercontent.com">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
