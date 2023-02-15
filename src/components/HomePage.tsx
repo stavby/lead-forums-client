2ff16af3d18954091c623f33b337c1ebf227aed0
lior <liorp10087@gmail.com>
2023-02-16 00:18:31 +0200

    done on card

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -3,7 +3,16 @@ import PostPeekCard from './PostPeek';
 
 const HomePage = () => (
   <>
-    <PostPeekCard />
+    {/* <PostPeekCard
+      post={{
+        id: 1,
+        author: { displayName: 'asdf', pictureUrl: '' },
+        topic: { id: 1, name: 'מנהיגות' },
+        title: 'תחרות',
+        content: 'האקתון  איזה כיהאקתון  איזה כיהאקתון  איזה כיף',
+        uploadDate: new Date(),
+      }}
+    /> */}
   </>
 );
 
--- src/components/PostPeek.tsx
+++ src/components/PostPeek.tsx
@@ -4,33 +4,50 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import * as React from 'react';
+import { post } from '../types';
 
 const styles = {
+  cardSizes: { width: 360, height: 100 },
   profilePicture: { width: '20%', marginRight: '10px' },
   postContent: {
     cardStyle: {
-        direction: 'rtl',
-        padding: 2,
-        width: '60%',
-        marginRight: 0,
+      direction: 'rtl',
+      height: 100,
+      padding: '15px',
+      marginTop: '35px',
     },
     cardText: {
-        title: {
-            fontSize: '14px' },
-        content: {
-            fontSize: '11px' },
+      title: {
+        fontSize: 18,
+        height: 30,
+      },
+      content: {
+        fontSize: '11px',
+        maxHeight: 60,
+        width: 150,
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+        whiteSpace: 'nowrap',
+      },
     },
   },
   postUploadTime: { fontSize: '11px' },
 };
 
-const PostPeekCard = () => (
-  <Card
-    sx={{
-      maxWidth: 360,
-    }}
-  >
-    <CardActionArea sx={{ display: 'flex', flexDirection: 'row-reverse' }}>
+const PostPeekCard = ({
+  post: { title, content, uploadDate },
+}: {
+  post: post;
+}) => (
+  <Card sx={styles.cardSizes}>
+    <CardActionArea
+      sx={{
+        height: styles.cardSizes.height,
+        width: styles.cardSizes.width,
+        display: 'flex',
+        flexDirection: 'row-reverse',
+      }}
+    >
       <CardMedia
         component='img'
         src='resources/Ellipse.png'
@@ -38,16 +55,23 @@ const PostPeekCard = () => (
         sx={styles.profilePicture}
       />
       <CardContent sx={styles.postContent.cardStyle}>
-        <Typography gutterBottom sx={styles.postContent.cardText.title} component='div'>
-          האקתון 2023
+        <Typography
+          gutterBottom
+          sx={styles.postContent.cardText.title}
+          component='div'
+        >
+          {title}
         </Typography>
-        <Typography sx={styles.postContent.cardText.content} color='text.secondary'>
-          כמה כיף לי
+        <Typography
+          sx={styles.postContent.cardText.content}
+          color='text.secondary'
+        >
+          {content}
         </Typography>
       </CardContent>
       <CardContent sx={{ width: '25%', marginRight: 0 }}>
         <Typography sx={styles.postUploadTime} color='text.secondary'>
-          לפני 5 שעות
+          {uploadDate.toLocaleString()}
         </Typography>
       </CardContent>
     </CardActionArea>
